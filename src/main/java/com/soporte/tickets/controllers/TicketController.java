1787e26ab59e6c6674c166f82de12b851ebac23a
Ignacio Osorio <rororurodo@gmail.com>
2025-05-22 00:37:04 -0400

    clases listas, pasando a intentar conectar

--- src/main/java/com/soporte/tickets/controllers/TicketController.java
+++ src/main/java/com/soporte/tickets/controllers/TicketController.java
@@ -4,15 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.soporte.tickets.models.Ticket;
 import com.soporte.tickets.repository.TicketRepository;
 import com.soporte.tickets.service.TicketService;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class TicketController {
@@ -20,27 +19,29 @@ public class TicketController {
     private final TicketRepository ticketRepository;
     @Autowired
     private TicketService ticketservice;
-    TicketService accionTicket= new TicketService();
+    TicketService accionTicket = new TicketService();
 
-    TicketController(TicketRepository ticketRepository){
+    TicketController(TicketRepository ticketRepository) {
         this.ticketRepository = ticketRepository;
     }
 
     @GetMapping("/test")
-    public String holaMundo(){
+    public String holaMundo() {
         return "probando spring";
     }
 
     @GetMapping("/tickets")
-    public List<Ticket> mostrarTickets(){
+    public List<Ticket> mostrarTickets() {
         return accionTicket.obtenerTickets();
     }
 
-
     @GetMapping("/tickets/{id}")
-    public ResponseEntity<Ticket> traerTicket(@PathVariable int id){
+    public ResponseEntity<Ticket> traerTicket(@PathVariable int id) {
         return ResponseEntity.ok(ticketservice.traerTicket(id));
     }
-    }
-    
 
+    @DeleteMapping("/tickets/{id}")
+    public String borrarTicket(@PathVariable int id) {
+        return accionTicket.borrarTicket(id);
+    }
+}
