7139de1bec72fd926fff564b4b4e000e219b0a2b
Ignacio Osorio <rororurodo@gmail.com>
2025-05-21 22:35:59 -0400

    Se agregago con exito la dependencia

--- src/main/java/com/soporte/tickets/repository/TicketRepository.java
+++ src/main/java/com/soporte/tickets/repository/TicketRepository.java
@@ -1,12 +1,16 @@
 package com.soporte.tickets.repository;
 
-import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.soporte.tickets.models.entity.TicketEntity;
 
 @Repository
-public class TicketRepository extends JpaRepository<TicketEntity,Integer> {
+public interface TicketRepository extends JpaRepository<TicketEntity, Integer> {
+
     TicketEntity findById(int id);
+
     boolean existsById(int id);
+
     void deleteById(int id);
 }
--- src/main/java/com/soporte/tickets/service/TicketService.java
+++ src/main/java/com/soporte/tickets/service/TicketService.java
@@ -9,45 +9,44 @@ import org.springframework.stereotype.Service;
 import com.soporte.tickets.models.Ticket;
 import com.soporte.tickets.models.entity.TicketEntity;
 import com.soporte.tickets.repository.TicketRepository;
+//import com.soporte.tickets.repository.TicketRepository;
 
 @Service
 public class TicketService {
 
-@Autowired
-private TicketRepository ticketRepository;
-private final List<Ticket> tickets = new ArrayList<>();
+    @Autowired
+    private TicketRepository ticketRepository;
+    private final List<Ticket> tickets = new ArrayList<>();
 
+    public TicketService() {
+        tickets.add(new Ticket(1, "", ""));
+    }
 
-public TicketService(){
-    tickets.add(new Ticket(1,"",""));
-}
+    public List<Ticket> obtenerTickets() {
+        return tickets;
+    }
 
-public List<Ticket> obtenerTickets(){
-    return tickets;
-}
+    public Ticket traerTicket(int id) {
+        try {
+            TicketEntity ticket = ticketRepository.findById(id);
+            if (ticket != null) {
+                Ticket ticketNuevo = new Ticket(ticket.getId(), ticket.getTitulo(), ticket.getDescripcion());
+                return ticketNuevo;
+            }
+            return null;
+        } catch (Exception e) {
+            return null;
+        }
+    }
 
-public Ticket traerTicket(int id){
-    try{
-        TicketEntity ticket = ticketRepository.findById(id);
-        if(ticket!=null){
-            Ticket ticketNuevo = new Ticket(ticket.getId(), ticket.getTitulo(), ticket.getDescripcion());
-            return ticketNuevo;
+    public String borrarTicket(int id) {
+        for (Ticket tick : tickets) {
+            if (tick.getId() == id) {
+                tickets.remove(tick);
+                return "ticket removido con exito";
+            }
         }
         return null;
     }
-    catch(Exception e){
-        return null;
-    }
-}
-public String borrarTicket(int id){
-    for (Ticket tick : tickets){
-        if (tick.getId()==id){
-            tickets.remove(tick);
-            return "ticket removido con exito";
-        }
-    }
-    return null;
-}
-
 
 }
